a5e62fc30b8cd8f50a15e750a7a2d7c07179975f
Ben Buzzelli <benbuzz68@gmail.com>
2020-09-01 10:44:20 -0400

    Fixed sql

--- plugins/omar-video-plugin/grails-app/init/omar/video/VideoBootStrap.groovy
+++ plugins/omar-video-plugin/grails-app/init/omar/video/VideoBootStrap.groovy
@@ -9,8 +9,8 @@ class VideoBootStrap {
 
     def init = { servletContext ->
       def sql = new Sql(dataSource)
-      
-      sql?.executeUpdate "create index if not exists raster_entry_ground_geom_idx on raster_entry using gist ( ground_geom )"
+
+      sql?.executeUpdate "create index if not exists video_data_set_ground_geom_idx on video_data_set using gist ( ground_geom )"
       sql?.close()
 
     }
