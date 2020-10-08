adda787182d1097addb68880054cec3aba1e19e5
Craig Schneider <craig.schneider@maxar.com>
2020-10-08 12:31:17 -0400

    changed err msg

--- plugins/omar-video-plugin/grails-app/init/omar/video/VideoBootStrap.groovy
+++ plugins/omar-video-plugin/grails-app/init/omar/video/VideoBootStrap.groovy
@@ -17,10 +17,9 @@ class VideoBootStrap {
             sql?.executeUpdate "create index if not exists video_data_set_ground_geom_idx on video_data_set using gist ( ground_geom )"
         }
         catch (final SQLException e) {
-            log.error("Bootstrap init failure. If the exception is from Hibernate unable to create raster_entry, " +
+            log.error(e.message + "; Bootstrap init failure. If unable to create video_data_set, " +
                     "the likely cause is missing postgis extensions/schemas. Omar Video plugin " +
                     "requires postgis DB schema.")
-            log.error(e.message)
             throw (e)
         }
         finally {
